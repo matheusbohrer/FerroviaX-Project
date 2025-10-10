966246ea5352df2b08538243864eb38ea61db251
felipelucasbdosreis <felipe_l_reis@estudante.sesisenai.org.br>
2025-10-10 11:35:55 -0300

    update código sql banco de dados

--- sql/ferroviax.sql
+++ sql/ferroviax.sql
@@ -7,6 +7,9 @@
 -- Versão do servidor: 8.0.21
 -- Versão do PHP: 8.2.12
 
+CREATE DATABASE ferroviax;
+USE ferroviax;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
