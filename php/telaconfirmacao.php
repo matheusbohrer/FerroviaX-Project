39419219fa09f7c49f74cdad1629b7f2b0d1fbaf
felipelucasbdosreis <felipe_l_reis@estudante.sesisenai.org.br>
2025-10-27 08:18:54 -0300

    add tela confirmação após cadastro

---
+++ php/telaconfirmacao.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>FerroviaX</title>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../css/telainicial.css">
+</head>
+
+<body>
+  <div class="splash-screen">
+    <div class="splash-content">
+      <img src="../imagens/logoBranca.png" alt="FerroviaX Logo">
+      <p>Seu cadastro foi realizado com sucesso!</p>
+    </div>
+  </div>
+
+  <script>
+  setTimeout(() => {
+    window.location.href = "telalogin.php";
+  }, 3000);
+</script>
+</body>
+
+</html>
